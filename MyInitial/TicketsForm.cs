70b0945eb59481aeee528468b7edbb9aebd09b34
001184032 <ariel.ferrante@student.tafesa.edu.au>
2025-03-12 00:34:55 +1030

    commit the back stall ticket changes

--- MyInitial/TicketsForm.cs
+++ MyInitial/TicketsForm.cs
@@ -39,6 +39,8 @@ namespace Ticketing
                 { mSection = 2; }
             if (radBox.Checked)
                 { mSection = 3; }
+            if (radBack.Checked) // Add radBack
+                { mSection = 4; }
 
             mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
 
